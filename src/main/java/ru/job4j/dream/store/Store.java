b8e8d076a1b7362197d319730ad8d8a2e6c681fa
kirillbelyaev74 <kirbel74@gmail.com>
2021-02-11 17:55:19 +0500

    4. candicates.jsp - список кандидатов.[276522#449895]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -1,5 +1,6 @@
 package ru.job4j.dream.store;
 
+import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 import java.util.Collection;
 import java.util.GregorianCalendar;
@@ -12,17 +13,26 @@ public class Store {
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java", "Вакансия на должность программиста Junior", new GregorianCalendar()));
         posts.put(2, new Post(2, "Middle Java", "Ваканисия на должность программиста Middle", new GregorianCalendar()));
-        posts.put(3, new Post(3, "Senior Java", "Ваканисия на должность программиста Middle", new GregorianCalendar()));
+        posts.put(3, new Post(3, "Senior Java", "Ваканисия на должность программиста Senior", new GregorianCalendar()));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -41,7 +41,7 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <% for (Post post : Store.instOf().findAll()) { %>
+                    <% for (Post post : Store.instOf().findAllPosts()) { %>
                     <tr>
                         <th scope="row"><%= post.getId()%>
                         </th>
